099862b524ccdd3e76a4dcb6136e1929affe1f6f
jORDYtORRES_13 <jordy.alegria204@std.edu.ni>
2025-06-14 16:54:26 -0600

    PRIMER CAMBIO CONFIRMADO

---
+++ Git_basic.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+ public static void main(String[] args) {
+ String nombre = "Estudiante";
+ int a = 5;
+ int b = 3;
+ int suma = a + b;
+ System.out.println("Hola, " + nombre + "!");
+ System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+ }
+ }
