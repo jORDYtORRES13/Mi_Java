8b2d7c415911f47248c90daa30cfbf44936a7520
jORDYtORRES_13 <jordy.alegria204@std.edu.ni>
2025-06-14 17:26:31 -0600

    TERCER CAMBIO CONFIRMADO

--- Git_basic.java
+++ Git_basic.java
@@ -4,7 +4,9 @@ public class Git_basic {
  int a = 10;
  int b = 13;
  int suma = a + b;
+ int mult = a * b;
  System.out.println("Hola, " + nombre + "!");
  System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+ System.out.println("La multiplicación de "+ a +" * "+ b +" es :  "+ mult);
  }
  }
