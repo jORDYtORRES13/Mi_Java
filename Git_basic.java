60f3783cf5573edc55fd001fe769ca2e49c8b190
jORDYtORRES_13 <jordy.alegria204@std.edu.ni>
2025-06-14 17:16:50 -0600

    SEGUNDO CAMBIO CONFIRMADO

--- Git_basic.java
+++ Git_basic.java
@@ -1,8 +1,8 @@
-public class HolaMundo {
+public class Git_basic {
  public static void main(String[] args) {
  String nombre = "Estudiante";
- int a = 5;
- int b = 3;
+ int a = 10;
+ int b = 13;
  int suma = a + b;
  System.out.println("Hola, " + nombre + "!");
  System.out.println("La suma de " + a + " + " + b + " es: " + suma);
