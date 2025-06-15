e1adcbc6fe05300c32465aa1a1df604e7d6e70da
jORDYtORRES_13 <jordy.alegria204@std.edu.ni>
2025-06-15 15:00:02 -0600

    CAMBIO SALUDO

--- Git_basic.java
+++ Git_basic.java
@@ -1,11 +1,13 @@
 public class Git_basic {
  public static void main(String[] args) {
- String nombre = "Estudiante";
+ String nombre = "Jordy Gadiel Alegría Torres";
+ int carnet = 20253554;
  int a = 10;
  int b = 13;
  int suma = a + b;
  int mult = a * b;
- System.out.println("Hola, " + nombre + "!");
+ System.out.println("Hola, mi nombre es " + nombre + "!");
+ System.out.println("Mi número de carnet es : "+ carnet);
  System.out.println("La suma de " + a + " + " + b + " es: " + suma);
  System.out.println("La multiplicación de "+ a +" * "+ b +" es :  "+ mult);
  }
